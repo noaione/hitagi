db9c30334793fe03c8455805d67e662ccb8fa1fb
noaione <noaione0809@gmail.com>
2023-11-03 13:32:22 +0700

    Change admin guard to plugins

---
+++ plugins/admin-middleware.ts
@@ -0,0 +1,27 @@
+export default defineNuxtPlugin(() => {
+  const settings = useServerMeta();
+
+  addRouteMiddleware(
+    "admin-guard",
+    (to) => {
+      // check if path is admin prefix (and ensure that is not just /admin or /admin/)
+      const isAdminIndex = to.path === "/admin" || to.path === "/admin/";
+      if (!to.path.startsWith("/admin") || isAdminIndex) {
+        return;
+      }
+
+      const redirect = encodeURIComponent(to.fullPath);
+
+      if (isNone(settings.apiKey64)) {
+        return navigateTo("/login?redirect=" + redirect);
+      }
+
+      if (settings.apiKey64.trim().length === 0) {
+        return navigateTo("/login?redirect=" + redirect);
+      }
+
+      // Is admin, continue
+    },
+    { global: true }
+  );
+});
